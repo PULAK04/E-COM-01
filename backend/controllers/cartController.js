a3e9c884dc2c028c225491805fda1cfc9ca3106f
PULAK04 <shayanadhikary25@gmail.com>
2026-04-08 19:09:01 +0530

    added function remove , update , get cart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -29,3 +29,74 @@ export const addToCart=async(req,res)=>{
         res.status(500).json({message:"server error",error})
     }
 }
+
+
+// remove item from cart 
+
+export const removeItem=async(req,res)=>{
+    try {
+        const {userId,productId}=req.body;
+        const cart=await Cart.findOne({userId});
+
+        if(!cart){
+            return res.status(404).json({message:"cart not found"});
+        }
+
+        cart.items=cart.items.filter((i)=>i.productId.toString!==productId);
+
+        await cart.save();
+
+        res.json({
+            message:"Item removed from the cart",
+            cart
+        })
+    } catch (error) {
+        res.status(500).json({message:"server error ",error})
+    }
+}
+
+
+export const updateQuantity=async(req,res)=>{
+    try {
+        const {userId,productId,quantity}=req.body;
+
+        const cart=await Cart.findOne({userId});
+
+        if(!cart){
+            return res.status(404).json({message:"cart not found"})
+        }
+
+        const item=cart.items.find((i)=>i.productId.toString()===productId)
+
+        if(!item){
+            return res.status(404).json({message:"item not found in cart"})
+        }
+
+        item.quantity=quantity;
+
+        await cart.save();
+
+        res.json({
+            message:"item quantity updated",
+            cart
+        })
+    } catch (error) {
+        res.json({
+            message:"server error",error
+        })
+    }
+}
+
+
+// get cart by userId
+export const getCart = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const cart = await Cart.findOne({ userId }).populate("items.productId");
+
+    res.json(cart);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error });
+  }
+};
