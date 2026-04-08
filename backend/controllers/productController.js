147c2396adfb480772369a4af52a221f769e3454
PULAK04 <shayanadhikary25@gmail.com>
2026-04-08 15:57:32 +0530

    initial commit

---
+++ backend/controllers/productController.js
@@ -0,0 +1,72 @@
+import Product from "../models/product.js";
+
+// Create a new product
+
+export const createProduct=async(req,res)=>{
+    try{
+        const product=await Product.create(req.body);
+        res.json({
+            message:"Product created successfully",
+            product
+        })
+    }catch(error){
+        res.status(500).json({message:"Server error",error})
+    }
+
+}
+
+
+// getting all products
+
+export const getProducts=async(req,res)=>{
+    try {
+        const {search,category}=req.query;
+        let filter={};
+
+        if(search){
+            filter.title={$regex:search,$options:"i"}
+        }
+
+        if(category){
+            filter.category=category;
+        }
+
+        const products=await Product.find(filter).sort({createdAt:-1});
+        res.json(products)
+    } catch (error) {
+        res.status(500).json({message:"Server error",error})
+    }
+}
+
+
+// update a product;
+
+
+export const updateProduct=async(req,res)=>{
+    try {
+        const updated=await Product.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true}
+        )
+
+        res.json({
+            message:"Product created successfully",
+            updated,
+        })
+    } catch (error) {
+        res.status(500).json({message:'server error',error});
+    }
+}
+
+
+// delete a product
+
+export const deleteProduct=async(req,res)=>{
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.json({message:"product deleted successfully"})
+    } catch (error) {
+        res.status(500).json({message:'server error ',error})
+    }
+}
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from 'react'
+import api from '../api/axios'
+
+const Signup = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Signup
