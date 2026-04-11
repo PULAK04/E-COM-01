a473a8aedf17992ba772b35290729ba9c84e95c7
PULAK04 <shayanadhikary25@gmail.com>
2026-04-11 17:27:56 +0530

    cart router added

--- backend/routes/address.js
+++ backend/routes/address.js
@@ -5,6 +5,6 @@ import {saveAddress,getAddresses} from "../controllers/addressController.js"
 const router=express.Router();
 
 router.post('/add',saveAddress)
-router.post('/:userId',getAddresses)
+router.get('/:userId',getAddresses)
 
 export default router;
